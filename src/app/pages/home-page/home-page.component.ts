fea842db191598bbdb24d45f23822ac7676ac05d
Wesley T. Benette <weluonf@gmail.com>
2024-05-07 12:52:40 -0300

    🎨 art(HomePage): Creates HomePage layout
    Construction and styling of the home page.
    Addition of icons and logo.
    
    Reviewed-by: WesleyTelesBenette.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+@Component
+({
+	selector: 'app-home-page',
+	standalone: true,
+	imports: [],
+	templateUrl: './home-page.component.html',
+	styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
 
