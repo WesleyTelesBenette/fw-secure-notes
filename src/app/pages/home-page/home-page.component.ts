f3146688fbbdbe8ac909aea8d20cd0fe5e054ae9
Wesley T. Benette <weluonf@gmail.com>
2024-05-07 12:58:22 -0300

    ✨ feat: Add Footer component
    Reviewed-by: WesleyTelesBenette.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { FooterComponent } from '../../components/footer/footer.component';
+
 @Component
 ({
 	selector: 'app-home-page',
 	standalone: true,
-	imports: [],
+	imports: [FooterComponent],
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss'
 })
