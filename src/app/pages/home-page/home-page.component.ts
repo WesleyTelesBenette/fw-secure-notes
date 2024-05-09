92837e68c9daa372b33acf17e08fd4e069187bc6
Wesley T. Benette <weluonf@gmail.com>
2024-05-09 15:49:07 -0300

    🔁 update(HomePage): Filters main input field
    Filters the main input field, preventing you from entering anything other than letters, numbers, and hyphens.
    
    Reviewed-by: WesleyTelesBenette.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,67 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { FooterComponent } from '../../components/footer/footer.component';
 
+/** Componente da página principal da aplicação.
+ *
+ * Usado como a pagina padrão da rota raiz do site.
+ */
 @Component
 ({
 	selector: 'app-home-page',
 	standalone: true,
-	imports: [FooterComponent],
+	imports: [FooterComponent, FormsModule],
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent
+{
+	/** Atributo que representa o conteúdo do ***input*** principal.
+	 *
+	 * Ele é (e deve ser) sincronizado pelo [(ngModel)].
+	 *
+	 * @type string
+	 */
+	pageName: string = '';
 
+	/** Método que delimita os caracteres que podem ser utilizados para o nome de uma página.
+	 *
+	 * Ele filtra e altera diretamente o valor do conteúdo do ***input***.
+	 *
+	 * @returns void
+	 */
+	public limitPageName(): void
+	{
+		let newPageName: string = this.pageName;
+
+		//Todas as letras, números e o hífen -> /[^a-zA-Z0-9-]
+		newPageName = newPageName.replace(/[^a-zA-Z0-9-]/g, '-');
+
+		this.pageName = newPageName;
+	}
+
+	/**
+	 *
+	 */
+	public createNote(): void
+	{
+		const inputContent: string | undefined | null = document.getElementById('input')?.textContent;
+
+		if (inputContent) alert(inputContent);
+
+		return;
+	}
+
+	/**
+	 *
+	 */
+	public searchNote(): void
+	{
+		const inputContent: string | undefined | null = document.getElementById('input')?.textContent;
+
+		if (inputContent) alert(inputContent);
+
+		return;
+	}
 }
